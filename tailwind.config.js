6207cf70251c022433f2a4147d280552fdacf986
Abdussomad20 <harzkid2018@gmail.com>
2023-01-08 11:48:32 +0100

    my hynitr web

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens:{
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
